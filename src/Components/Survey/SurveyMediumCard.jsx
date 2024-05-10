e30e3c9b72541e3cc72b99c7b537e8f492b9080a
dadulkaticha <dagmar.ticha@unob.cz>
2024-05-10 08:30:38 +0200

    Date to string

--- src/Components/Survey/SurveyMediumCard.jsx
+++ src/Components/Survey/SurveyMediumCard.jsx
@@ -4,6 +4,7 @@ import Col from 'react-bootstrap/Col'
 import { SurveyLink } from './SurveyLink'
 
 export const SurveyMediumCard = ({survey}) => {
+    const last = new Date(survey?.lastchange).toDateString()
     return (
         <CardCapsule  title={<>Událost <SurveyLink survey={survey } /></>}>
 
@@ -13,7 +14,7 @@ export const SurveyMediumCard = ({survey}) => {
             </Row>
             <Row>
                 <Col>Změna</Col>
-                <Col>{survey?.lastchange}</Col>
+                <Col>{last}</Col>
             </Row>
             <Row>
                 <Col>Otázky</Col>
